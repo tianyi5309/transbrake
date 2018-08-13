8263ba5e8574bc110943175a3ba0ecfdfdaaf1aa
tianyi <>
2018-08-12 20:42:00 -0500

    working

--- transcode.py
+++ transcode.py
@@ -21,23 +21,9 @@ if os.path.isdir(torrent_full):
         subprocess.check_output(['mkdir', out_prefix])
     
     for movie in movies:
-        movie_name = out_prefix + vmovie.split('/')[-1].split('.')[0] + '.mp4'
+        movie_name = out_prefix + movie.split('/')[-1].split('.')[0] + '.mp4'
         command = ['HandBrakeCLI', '-i', movie, '-o', movie_name, '-e', 'x265', '-q', '20', '-B', '160', '--encoder-preset', 'faster']
-        print(command)
         subprocess.check_output(command)
 else:
-    movie_name = '/home/Movies/' + vmovie.split('/')[-1].split('.')[0] + '.mp4'
+    movie_name = '/home/Movies/' + movie.split('/')[-1].split('.')[0] + '.mp4'
     subprocess.check_output(['HandBrakeCLI', '-i', torrent_full, '-o', movie_name, '-e', 'x265', '-q', '20', '-B', '160', '--encoder-preset', 'faster'])
-
-# movies = []
-# movies.extend(glob.iglob(torrent_dir + '/**/*.mkv', recursive=True))
-# movies.extend(glob.iglob(torrent_dir + '/**/*.mp4', recursive=True))
-# 
-# out_prefix = '/home/Movies/'
-# if len(movies) > 1:
-#     out_prefix += torrent_name + '/'
-# 
-# for movie in movies:
-#     movie_name = out_prefix + vmovie.split('/')[-1].split('.')[0] + '.mp4'
-#     print('HandBrakeCLI', '-i', movie, '-o', movie_name, '-e', 'x265', '-q', '20', '-B', '160', '--encoder-preset', 'faster')
-#     # subprocess.check_output(["rm", "sra-download-out/" + seq + ".fastq"])
--- transcode.sh
+++ transcode.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-# echo "You shall not pass!" > /tmp/test.txt
-# echo "$TR_TORRENT_DIR" > /tmp/dir.txt
-# echo "$TR_TORRENT_NAME" > /tmp/name.txt
-# which python3 > /tmp/py3ver.txt
-python3 /home/Torrents/transbrake/transcode.py "$TR_TORRENT_DIR" "$TR_TORRENT_NAME" >> /tmp/log.txt
+python3 /home/Torrents/transbrake/transcode.py "$TR_TORRENT_DIR" "$TR_TORRENT_NAME" > /tmp/log.txt
