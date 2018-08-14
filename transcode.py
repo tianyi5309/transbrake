2177d33f03a5c7dfd2a5f9f7a608851c44b3f649
tianyi <>
2018-08-14 18:09:21 -0500

    add encoding and filter bad subtitles

--- transcode.py
+++ transcode.py
@@ -12,10 +12,19 @@ torrent_name = sys.argv[2]
 torrent_full = torrent_dir + '/' + torrent_name
 
 def encode(inmov, outmov):
-    print('Copying movie ' + inmov + ' to ' + outmov)
-    subprocess.check_output(['ffmpeg', '-i', inmov, '-map', '0', '-map', '-0:m:language:rus?', '-map', '-0:m:language:ukr?', '-vcodec', 'copy', '-acodec', 'aac', '-b:a', '256k', '-map_metadata', '-1', '-scodec', 'copy', outmov])
+    print('Encoding movie ' + inmov + ' to ' + outmov)
+    cpu = psutil.cpu_percent(interval=10)
+    print('Current CPU: ', cpu)
+    while cpu > 50:
+        print('Waiting until cpu usage decreases (' + str(cpu) + ')')
+        cpu = psutil.cpu_percent(interval=120)
+    subprocess.check_output(['ffmpeg', '-i', inmov, '-map', '0', '-map', '-0:m:language:rus?', '-map', '-0:m:language:ukr?', '-map', '-0:m:type:hdmv_pgs_subtitle', '-vcodec', 'libx264', '-crf', '18', '-maxrate', '8M', '-bufsize', '8M', '-preset', 'fast', '-acodec', 'aac', '-b:a', '256k', '-map_metadata', '-1', '-scodec', 'copy', '-movflags', 'faststart', outmov])
+    
+    # print('Copying movie ' + inmov + ' to ' + outmov)
+    # subprocess.check_output(['ffmpeg', '-i', inmov, '-map', '0', '-map', '-0:m:language:rus?', '-map', '-0:m:language:ukr?', '-vcodec', 'copy', '-acodec', 'aac', '-b:a', '256k', '-map_metadata', '-1', '-scodec', 'copy', outmov])
 
     # subprocess.check_output(['mv', inmov, outmov]) # copy file
+    
     # print('Encoding movie ' + inmov + ' to ' + outmov)
     # cpu = psutil.cpu_percent(interval=10)
     # print('Current CPU: ', cpu)
@@ -46,10 +55,10 @@ if os.path.isdir(torrent_full):
             os.makedirs(out_prefix)
         
     for movie in movies:
-        movie_name = out_prefix + ' '.join(movie.split('/')[-1].split('.')[:-1]) + '.mkv'
+        movie_name = out_prefix + ' '.join(movie.split('/')[-1].split('.')[:-1]) + '.mp4'
         encode(movie, movie_name)
 else:
     # Single file
     print('Single movie: ', torrent_full)
-    movie_name = '/home/Movies/' + ' '.join(torrent_full.split('/')[-1].split('.')[:-1]) + '.mkv'
+    movie_name = '/home/Movies/' + ' '.join(torrent_full.split('/')[-1].split('.')[:-1]) + '.mp4'
     encode(torrent_full, movie_name)
