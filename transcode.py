b0ee7372233e26262b2348dadcb38ecfb9e16ccd
tianyi <>
2018-08-16 01:35:27 -0500

    allow concurrent transcodes, implement logging

--- transcode.py
+++ transcode.py
@@ -9,20 +9,31 @@ import json
 
 # Reminder: set permissions to write to /Movies, /TV Shows, /Torrents
 
-torrent_dir = sys.argv[1]
 torrent_name = sys.argv[2]
 torrent_full = '/home/Torrents/' + torrent_name
 
-# torrent_full = torrent_dir + '/' + torrent_name
+class Logger:
+    def __init__(self, filename):
+        self.outf = open(filename, 'a')
+        self.record = ''
+    def write(self, *args):
+        s = ' '.join([str(i) for i in args])
+        print(s)
+        self.record += s + '\n'
+    def save(self):
+        self.outf.write(self.record)
+        self.record = ''
+
+log = Logger('/home/Torrents/log.txt')
 
 def encode(inmov, outmov):
-    print('Encoding movie ' + inmov + ' to ' + outmov)
-    cpu = psutil.cpu_percent(interval=3)
-    print('Current CPU: ', cpu)
-    while cpu > 50:
-        print('Waiting until cpu usage decreases (' + str(cpu) + ')')
-        sleep(300)
-        cpu = psutil.cpu_percent(interval=0.5)
+    log.write('Encoding movie ' + inmov + ' to ' + outmov)
+    # cpu = psutil.cpu_percent(interval=3)
+    # print('Current CPU: ', cpu)
+    # while cpu > 50:
+    #     print('Waiting until cpu usage decreases (' + str(cpu) + ')')
+    #     sleep(300)
+    #     cpu = psutil.cpu_percent(interval=0.5)
     
     # Parse input streams
     streams = json.loads(subprocess.check_output(['ffprobe', '-v', 'quiet', '-print_format', 'json', '-show_streams', inmov]).decode())['streams']
@@ -32,7 +43,7 @@ def encode(inmov, outmov):
     for stream in streams:
         if 'codec_name' in stream and stream['codec_name'] in filtered_codecs:
             filtered_streams.append(stream['index'])
-    print('Filtering streams', filtered_streams)
+    log.write('Filtering streams', filtered_streams)
     filters = []
     for streamn in filtered_streams:
         filters.append('-map')
@@ -40,19 +51,6 @@ def encode(inmov, outmov):
     
     # Transcode
     subprocess.check_output(['ffmpeg', '-i', inmov, '-map', '0', '-map', '-0:m:language:rus?', '-map', '-0:m:language:ukr?'] + filters + ['-vcodec', 'libx264', '-x264-params', 'analyse=none:ref=1:rc-lookahead=30', '-crf', '18', '-maxrate', '8M', '-bufsize', '8M', '-preset', 'fast', '-tune', 'film', '-acodec', 'aac', '-b:a', '256k', '-map_metadata', '-1', '-scodec', 'mov_text', '-movflags', 'faststart', outmov])
-    
-    # print('Copying movie ' + inmov + ' to ' + outmov)
-    # subprocess.check_output(['ffmpeg', '-i', inmov, '-map', '0', '-map', '-0:m:language:rus?', '-map', '-0:m:language:ukr?', '-vcodec', 'copy', '-acodec', 'aac', '-b:a', '256k', '-map_metadata', '-1', '-scodec', 'copy', outmov])
-
-    # subprocess.check_output(['mv', inmov, outmov]) # copy file
-    
-    # print('Encoding movie ' + inmov + ' to ' + outmov)
-    # cpu = psutil.cpu_percent(interval=10)
-    # print('Current CPU: ', cpu)
-    # while cpu > 50:
-        # print('Waiting until cpu usage decreases (' + str(cpu) + ')')
-        # cpu = psutil.cpu_percent(interval=120)
-    # subprocess.check_output(['HandBrakeCLI', '-i', inmov, '-o', outmov, '-e', 'x264', '-q', '16', '-B', '160', '--encoder-preset', 'fast', '--all-subtitles', '-O', '--all-audio'])
 
 if os.path.isdir(torrent_full):
     # Torrent folder
@@ -60,31 +58,29 @@ if os.path.isdir(torrent_full):
     movies.extend(glob.iglob(torrent_full + '/**/*.mkv', recursive=True))
     movies.extend(glob.iglob(torrent_full + '/**/*.mp4', recursive=True))
     movies.extend(glob.iglob(torrent_full + '/**/*.avi', recursive=True))
-    print('Movies: ', movies)
+    log.write('Movies: ', movies)
     
     torrent_name = torrent_name.replace('.', ' ')
     
     if len(movies) == 1:
         # Single movie
         out_prefix = '/home/Movies/' + torrent_name + '/'
-        print('Movie identified, saving to ', out_prefix)
-        if not os.path.exists(out_prefix):
-            os.makedirs(out_prefix)
+        log.write('Movie identified, saving to ', out_prefix)
     else:
         # TV series
         out_prefix = '/home/TV Shows/' + torrent_name + '/'
-        print('TV Series identified, saving to ', out_prefix)
+        log.write('TV Series identified, saving to ', out_prefix)
+        
         if not os.path.exists(out_prefix):
-            # print('Directory does not exist, creating ...')
                 os.makedirs(out_prefix)
-            # print('Directory created')
-        # print('dealt with directory stuff')
         
     for movie in movies:
         movie_name = out_prefix + ' '.join(movie.split('/')[-1].split('.')[:-1]) + '.mp4'
         encode(movie, movie_name)
 else:
     # Single file
-    print('Single movie: ', torrent_full)
+    log.write('Single movie: ', torrent_full)
     movie_name = '/home/Movies/' + ' '.join(torrent_full.split('/')[-1].split('.')[:-1]) + '.mp4'
     encode(torrent_full, movie_name)
+
+log.save()
--- transcode.sh
+++ transcode.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 /home/Torrents/transbrake/transcode.py "$TR_TORRENT_DIR" "$TR_TORRENT_NAME" > /tmp/log.txt
+python3 /home/Torrents/transbrake/transcode.py "$TR_TORRENT_DIR" "$TR_TORRENT_NAME"
