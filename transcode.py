7bbd93d3c3a069016600ef68b7673b7b695b928e
tianyi5309 <mail@tianyi.io>
2018-12-26 18:19:10 -0600

    add exclusion for dvd_subtitle and removed error reporting

--- transcode.py
+++ transcode.py
@@ -58,7 +58,7 @@ def encode(inmov, outdir, outname):
     for stream in streams:
         if stream['codec_type'] == 'subtitle':
             banned_languages = ['ukr', 'rus']
-            banned_codecs = ['hdmv_pgs_subtitle']
+            banned_codecs = ['hdmv_pgs_subtitle', 'dvd_subtitle']
             if 'tags' in stream and 'language' in stream['tags'] and stream['tags']['language'] not in banned_languages and stream['codec_name'] not in banned_codecs:
                 chosen_streams_numbers.append(stream['index'])
     
@@ -77,7 +77,7 @@ def encode(inmov, outdir, outname):
     
     # Transcode
     start = time.time()
-    subprocess.check_output(['ffmpeg', '-i', inmov] + chosen_streams + ['-vcodec', 'libx264', '-profile:v', 'high', '-level', '4.2', '-pix_fmt', 'yuv420p', '-deinterlace', '-x264opts', 'analyse=none:rc-lookahead=30:ref=1:crf-max=25:qpmax=34', '-crf', '18', '-maxrate', '8M', '-bufsize', '16M', '-preset', 'fast', '-tune', 'film', '-filter:v', 'hqdn3d=1:1:4:4', '-acodec', 'aac', '-ac', '2', '-b:a', '192k', '-af', 'aresample=async=1000', '-scodec', 'mov_text', '-movflags', '+faststart', '-max_muxing_queue_size', '9999', outtmp, '-report'])
+    subprocess.check_output(['ffmpeg', '-i', inmov] + chosen_streams + ['-vcodec', 'libx264', '-profile:v', 'high', '-level', '4.2', '-pix_fmt', 'yuv420p', '-deinterlace', '-x264opts', 'analyse=none:rc-lookahead=30:ref=1:crf-max=25:qpmax=34', '-crf', '18', '-maxrate', '8M', '-bufsize', '16M', '-preset', 'fast', '-tune', 'film', '-filter:v', 'hqdn3d=1:1:4:4', '-acodec', 'aac', '-ac', '2', '-b:a', '192k', '-af', 'aresample=async=1000', '-scodec', 'mov_text', '-movflags', '+faststart', '-max_muxing_queue_size', '9999', outtmp])
     subprocess.check_output(['mv', outtmp, outmov])
     end = time.time()
     elapsed = end - start
